308636be59805b6d96dc906f2658f76de4a9ef1e
user <user@users-MacBook-Pro.local>
2025-08-01 14:36:46 +0530

    fix: Update AccordionSelectableList imports to use package imports instead of relative paths

--- now-design/packages/molecules/src/AccordionSelectableList/AccordionSelectableList.jsx
+++ now-design/packages/molecules/src/AccordionSelectableList/AccordionSelectableList.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 import PropTypes from 'prop-types';
-import { AccordionTrigger } from '../accordion-trigger';
-import { SelectableListItem } from '../selectable-list-item';
+import { AccordionTrigger } from 'now-design-atoms';
+import { SelectableListItem } from 'now-design-atoms';
 import './AccordionSelectableList.css';
 
 /**
