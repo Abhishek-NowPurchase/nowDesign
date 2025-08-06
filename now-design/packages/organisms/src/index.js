16773d43bbc1b31ea6c51f557d8c0dc4ccc3f524
user <user@users-MacBook-Pro.local>
2025-08-06 14:04:35 +0530

    refactor: Move LeftPanel from template to organisms package and remove template package

--- now-design/packages/organisms/src/index.js
+++ now-design/packages/organisms/src/index.js
@@ -1,1 +1,2 @@
-export { default as AccordionSelectableListContainer } from './AccordionSelectableListContainer/AccordionSelectableListContainer'; 
+export { default as AccordionSelectableListContainer } from './AccordionSelectableListContainer/AccordionSelectableListContainer';
+export { default as LeftPanel } from './leftPanel/LeftPanel'; 
