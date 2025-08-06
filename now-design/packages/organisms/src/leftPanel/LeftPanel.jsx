5c70b8fcec736a6d18716029c4df66fbdd08fe8d
user <user@users-MacBook-Pro.local>
2025-08-06 14:43:46 +0530

    fix: Correct circular dependency in LeftPanel import

--- now-design/packages/organisms/src/leftPanel/LeftPanel.jsx
+++ now-design/packages/organisms/src/leftPanel/LeftPanel.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { AccordionSelectableListContainer } from 'now-design-organisms';
+import { AccordionSelectableListContainer } from '../AccordionSelectableListContainer';
 import { LogoMetalCloud } from 'now-design-atoms';
 import './LeftPanel.css';
 
